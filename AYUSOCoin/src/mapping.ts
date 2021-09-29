49d61888725ad91c901a5e000de17e6ddf02ba50
Pumita <hola@kike.wtf>
2021-09-29 09:43:55 -0700

    :fire: A little bit easy...

--- AYUSOCoin/src/mapping.ts
+++ AYUSOCoin/src/mapping.ts
@@ -1,56 +1,85 @@
+import { Address, BigInt } from "@graphprotocol/graph-ts"
 import {
   Ayusocoin,
   Approval,
   SetRootAttempt,
-  Transfer
+  Transfer as TransferEvent
 } from "../generated/Ayusocoin/Ayusocoin"
-import { Account } from "../generated/schema"
+import { Account, Transfer } from "../generated/schema" 
+
+/*
+    __________
+   /          \
+  |  Entities  |
+   \__________/
+
+*/
+
+/**
+ * Ensures that the account entity is obtained correctly.
+ * @param id Account address
+ * @param adx Contract address
+ * @returns Account entity
+ */
+export function ensureAccount(id: Address, adx: Address): Account {
+  let account = Account.load(id.toHex())
+  if(!account) {
+      let contract = Ayusocoin.bind(adx)
+      account = new Account(id.toHex())
+      account.balance = BigInt.fromI32(0)
+      account.isRoot = contract.isRoot(id)
+  }
+  return account as Account
+}
+
+/*
+    __________
+   /          \
+  |   Events   |
+   \__________/
+
+*/
 
 export function handleApproval(event: Approval): void {
-  // - contract._totalSupply(...)
-  // - contract.allowance(...)
-  // - contract.allowed(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.decimals(...)
-  // - contract.getRoot(...)
-  // - contract.isRoot(...)
-  // - contract.maxbalance_per_addr(...)
-  // - contract.name(...)
-  // - contract.setMaxBalancePerAddress(...)
-  // - contract.setRoot(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
+  
 }
 
+/**
+ * Event handler related to changes in contract ownership
+ * @param event SetRootAttempt
+ */
 export function handleSetRootAttempt(event: SetRootAttempt): void {
-  console.log("[SetRoot] " + event.params.oldroot.toHex() + " -> " + event.params.newroot.toHex())
-  let _old = Account.load(event.params.oldroot.toHex())
-  if (!_old) _old = new Account(event.params.oldroot.toHex())
-  _old.isRoot = false
-  _old.save()
-
-  let _new = Account.load(event.params.newroot.toHex())
-  if (!_new) _new = new Account(event.params.newroot.toHex())
-  _new.isRoot = true
-  _new.save()
+  // Remove the contract owner
+  let oldr = ensureAccount(event.params.oldroot, event.address)
+  oldr.isRoot = false
+  oldr.save()
+  // Establish the new contract owner
+  let newr = ensureAccount(event.params.newroot, event.address)
+  newr.isRoot = true
+  newr.save()
 }
 
-export function handleTransfer(event: Transfer): void {
+/**
+ * Event handler related to token transfers
+ * @param event Transfer
+ */
+export function handleTransfer(event: TransferEvent): void {
+  // Contract
   let contract = Ayusocoin.bind(event.address)
-  let maxbalance_per_addr = contract.maxbalance_per_addr()
-
-  let from = Account.load(event.params._from.toHex())
-  if (!from) from = new Account(event.params._from.toHex())
+  // Establish the new transfer
+  let transfer = new Transfer(event.transaction.hash.toHex())
+  transfer.to = event.params._to.toHex()
+  transfer.from = event.params._from.toHex()
+  transfer.amount = event.params._value
+  transfer.timestamp = event.block.timestamp
+  transfer.gasUsed = event.block.gasUsed
+  transfer.gasLimit = event.block.gasLimit
+  transfer.save()
+  // Update the balance of the accounts
+  let from = ensureAccount(event.params._from, event.address)
   from.balance = contract.balanceOf(event.params._from)
-  from.capped = from.balance.ge(maxbalance_per_addr)
   from.save()
-
-  let to = Account.load(event.params._to.toHex())
-  if (!to) to = new Account(event.params._to.toHex())
+  let to = ensureAccount(event.params._to, event.address)
   to.balance = contract.balanceOf(event.params._to)
-  to.capped = to.balance.ge(maxbalance_per_addr)
   to.save()
 }
